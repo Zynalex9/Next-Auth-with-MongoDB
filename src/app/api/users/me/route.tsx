f133ed64bad5bffd632510bd4988d1b1d73e9fcd
Zynalex9 <alzn952@gmail.com>
2024-09-10 12:27:31 +0500

    Personal route

---
+++ src/app/api/users/me/route.tsx
@@ -0,0 +1,15 @@
+import { connectDB } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+
+export async function POST(request: NextRequest) {
+  const userId = await getDataFromToken(request);
+  const user = await User.findOne({ _id: userId }).select("-passsword");
+  return NextResponse.json({
+    message: "User found",
+    data: user,
+  });
+}
