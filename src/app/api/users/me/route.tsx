ed3bbebc4da1c94d2cd18bcec075aa07caf0ba49
Zynalex9 <alzn952@gmail.com>
2024-09-11 16:21:40 +0500

    profile

--- src/app/api/users/me/route.tsx
+++ src/app/api/users/me/route.tsx
@@ -5,7 +5,7 @@ import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { getDataFromToken } from "@/helpers/getDataFromToken";
 
-export async function POST(request: NextRequest) {
+export async function GET(request: NextRequest) {
   const userId = await getDataFromToken(request);
   const user = await User.findOne({ _id: userId }).select("-passsword");
   return NextResponse.json({
